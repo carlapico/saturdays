b5515de7d600db810abf779e2531a993456b3e63
Carla Pico <carlapico@Carlas-MacBook-Air.local>
2022-07-16 10:57:05 -0400

    initial commit for saturdays folder

---
+++ saturday1.js
@@ -0,0 +1,21 @@
+let population = 256
+let n= 2
+
+function futurePeople (population,n) {
+    let newPeople =n *360
+    let findPop = population + newPeople
+    return findPop
+}
+
+console.log(futurePeople(population,n))
+
+// same as 
+
+
+function futurePeople (population,n) {
+    let newPeople =n *360
+    let findPop = population + newPeople
+    return findPop
+}
+
+console.log(futurePeople(3248,6))
